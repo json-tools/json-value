ce44997f9f0025555004371e8b9bc72e864d68f9
Anatoliy <ac@ub.io>
2017-11-01 17:47:04 +0000

    Initial commit

---
+++ src/JsonValue.elm
@@ -0,0 +1,276 @@
+module JsonValue
+    exposing
+        ( JsonValue(ObjectValue, ArrayValue, BoolValue, StringValue, NumericValue, NullValue)
+        , decoder
+        , encode
+        , setIn
+        , getIn
+        , setPropertyName
+        )
+
+{-|
+# Definitions
+
+@docs JsonValue
+
+# Reading
+
+@docs getIn
+
+# Manipulation
+
+@docs setIn, setPropertyName
+
+# Transforms
+
+@docs decoder, encode
+
+-}
+
+import Json.Encode as Encode
+import Json.Decode as Decode exposing (Decoder, Value, succeed, decodeString)
+
+
+{-|
+    Type representing json value according to spec
+-}
+type JsonValue
+    = ObjectValue (List ( String, JsonValue ))
+    | ArrayValue (List JsonValue)
+    | BoolValue Bool
+    | NullValue
+    | NumericValue Float
+    | StringValue String
+
+
+{-|
+    Decoder for JsonValue
+-}
+decoder : Decoder JsonValue
+decoder =
+    let
+        objectValueDecoder =
+            Decode.keyValuePairs (Decode.lazy (\_ -> decoder))
+                |> Decode.andThen (List.reverse >> succeed)
+                |> Decode.map ObjectValue
+
+        arrayValueDecoder =
+            Decode.list (Decode.lazy (\_ -> decoder))
+                |> Decode.map ArrayValue
+    in
+        Decode.oneOf
+            [ objectValueDecoder
+            , arrayValueDecoder
+            , Decode.null NullValue
+            , Decode.string |> Decode.map StringValue
+            , Decode.float |> Decode.map NumericValue
+            , Decode.bool |> Decode.map BoolValue
+            ]
+
+
+{-|
+    Encoder for JsonValue
+-}
+encode : JsonValue -> Value
+encode v =
+    case v of
+        ObjectValue ov ->
+            ov
+                |> List.map (\( key, jv ) -> ( key, encode jv ))
+                |> Encode.object
+
+        ArrayValue av ->
+            av
+                |> List.map encode
+                |> Encode.list
+
+        StringValue s ->
+            Encode.string s
+
+        BoolValue b ->
+            Encode.bool b
+
+        NullValue ->
+            Encode.null
+
+        NumericValue n ->
+            Encode.float n
+
+
+{-|
+    Rename property in json value
+-}
+setPropertyName : ( List String, Int ) -> String -> JsonValue -> Result String JsonValue
+setPropertyName ( pathInJson, index ) newName hostValue =
+    let
+        renameKey val =
+            case val of
+                ObjectValue v ->
+                    v
+                        |> List.indexedMap
+                            (\i ( k, v ) ->
+                                ( if index == i then
+                                    newName
+                                  else
+                                    k
+                                , v
+                                )
+                            )
+                        |> ObjectValue
+                        >> Ok
+
+                _ ->
+                    Err "Can not rename property of this json value"
+
+        targetValue =
+            hostValue
+                |> getIn pathInJson
+                |> Result.andThen renameKey
+                |> Result.withDefault hostValue
+    in
+        setIn pathInJson targetValue hostValue
+
+
+{-|
+    Set json value at given path
+-}
+setIn : List String -> JsonValue -> JsonValue -> Result String JsonValue
+setIn pathInJson valueToSet hostValue =
+    let
+        path =
+            pathInJson
+                |> List.reverse
+
+        newValue =
+            case path of
+                [] ->
+                    Ok valueToSet
+
+                _ :: subpath ->
+                    path
+                        |> List.foldl
+                            (\key ( path, value ) ->
+                                let
+                                    p =
+                                        List.reverse path
+
+                                    v =
+                                        value
+                                            |> Result.andThen
+                                                (\vv ->
+                                                    hostValue
+                                                        |> getIn p
+                                                        |> Result.andThen (setProperty key vv)
+                                                )
+                                in
+                                    case path of
+                                        [] ->
+                                            ( [], v )
+
+                                        _ :: tail ->
+                                            ( tail, v )
+                            )
+                            ( subpath, Ok valueToSet )
+                        |> \( _, v ) -> v
+    in
+        newValue
+
+
+{-|
+    Get json value at given path
+-}
+getIn : List String -> JsonValue -> Result String JsonValue
+getIn path value =
+    case path of
+        [] ->
+            Ok value
+
+        head :: tail ->
+            case value of
+                ObjectValue v ->
+                    v
+                        |> List.foldl
+                            (\( key, v ) res ->
+                                if res /= Nothing then
+                                    res
+                                else if key == head then
+                                    Just v
+                                else
+                                    Nothing
+                            )
+                            Nothing
+                        |> Result.fromMaybe "Key not found"
+                        |> Result.andThen (getIn tail)
+
+                ArrayValue v ->
+                    head
+                        |> String.toInt
+                        |> Result.andThen
+                            (\index ->
+                                v
+                                    |> List.drop index
+                                    |> List.head
+                                    |> Result.fromMaybe "Index is too big"
+                            )
+                        |> Result.andThen (getIn tail)
+
+                _ ->
+                    Err "You are trying to access property of something that is not object or array"
+
+
+setProperty : String -> JsonValue -> JsonValue -> Result String JsonValue
+setProperty key value object =
+    let
+        updateOrAppend list =
+            if List.any (\( k, _ ) -> k == key) list then
+                list
+                    |> List.map
+                        (\( k, v ) ->
+                            if k == key then
+                                ( key, value )
+                            else
+                                ( k, v )
+                        )
+            else
+                list ++ [ ( key, value ) ]
+    in
+        case object of
+            ObjectValue o ->
+                o
+                    |> updateOrAppend
+                    |> ObjectValue
+                    |> Ok
+
+            ArrayValue list ->
+                let
+                    index =
+                        key
+                            |> decodeString Decode.int
+                            |> Result.withDefault (List.length list)
+                in
+                    if List.length list > index then
+                        list
+                            |> List.indexedMap
+                                (\i v ->
+                                    if i == index then
+                                        value
+                                    else
+                                        v
+                                )
+                            |> ArrayValue
+                            |> Ok
+                    else
+                        list
+                            ++ [ value ]
+                            |> ArrayValue
+                            |> Ok
+
+            _ ->
+                if key == "0" then
+                    [ value ]
+                        |> ArrayValue
+                        |> Ok
+                else
+                    [ ( key, value ) ]
+                        |> ObjectValue
+                        |> Ok
---
+++ tests/Example.elm
@@ -0,0 +1,194 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Test exposing (..)
+import JsonValue
+    exposing
+        ( JsonValue(ObjectValue, ArrayValue, BoolValue, StringValue, NumericValue, NullValue)
+        , setIn
+        , getIn
+        , setPropertyName
+        )
+import Json.Encode as Encode exposing (object, list, string, int, float, bool, null)
+import Json.Decode as Decode
+
+
+--import Fuzz exposing (Fuzzer, list, int, string)
+
+
+suite : Test
+suite =
+    describe "JsonValue"
+        [ test "decoder" <|
+            \() ->
+                [ ( "str", string "value" )
+                , ( "array", list [ int 10, float 0.1, bool False, null ] )
+                ]
+                    |> object
+                    |> Decode.decodeValue JsonValue.decoder
+                    |> Expect.equal
+                        (Ok <|
+                            ObjectValue
+                                [ ( "str", StringValue "value" )
+                                , ( "array", ArrayValue [ NumericValue 10, NumericValue 0.1, BoolValue False, NullValue ] )
+                                ]
+                        )
+        , test "encoder" <|
+            \() ->
+                [ ( "str", StringValue "value" )
+                , ( "array", ArrayValue [ NumericValue 10, NumericValue 0.1, BoolValue False, NullValue ] )
+                ]
+                    |> ObjectValue
+                    |> JsonValue.encode
+                    |> Expect.equal
+                        ([ ( "str", string "value" )
+                         , ( "array", list [ int 10, float 0.1, bool False, null ] )
+                         ]
+                            |> object
+                        )
+        , describe "setIn"
+            [ test "object" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "foo"
+                        |> setIn [ "foo" ] (StringValue "bar")
+                        |> Expect.equal
+                            (StringValue "bar"
+                                |> inObjWithProp "foo"
+                                |> Ok
+                            )
+            , test "slightly nested object" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "bar"
+                        |> inObjWithProp "foo"
+                        |> setIn [ "foo", "bar" ] (StringValue "baz")
+                        |> Expect.equal
+                            (StringValue "baz"
+                                |> inObjWithProp "bar"
+                                |> inObjWithProp "foo"
+                                |> Ok
+                            )
+            , test "slightly more nested object" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "bar"
+                        |> inObjWithProp "foo"
+                        |> inObjWithProp "root"
+                        |> setIn [ "root", "foo", "bar" ] (StringValue "baz")
+                        |> Expect.equal
+                            (StringValue "baz"
+                                |> inObjWithProp "bar"
+                                |> inObjWithProp "foo"
+                                |> inObjWithProp "root"
+                                |> Ok
+                            )
+            , test "deeply nested object" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "bar"
+                        |> inObjWithProp "foo"
+                        |> inObjWithProp "root"
+                        |> inObjWithProp "rootest"
+                        |> setIn [ "rootest", "root", "foo", "bar" ] (StringValue "baz")
+                        |> Expect.equal
+                            (StringValue "baz"
+                                |> inObjWithProp "bar"
+                                |> inObjWithProp "foo"
+                                |> inObjWithProp "root"
+                                |> inObjWithProp "rootest"
+                                |> Ok
+                            )
+            , test "quite deeply nested object" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "bar"
+                        |> inObjWithProp "foo"
+                        |> inObjWithProp "root"
+                        |> inObjWithProp "rootest"
+                        |> inObjWithProp "_"
+                        |> setIn [ "_", "rootest", "root", "foo", "bar" ] (StringValue "baz")
+                        |> Expect.equal
+                            (StringValue "baz"
+                                |> inObjWithProp "bar"
+                                |> inObjWithProp "foo"
+                                |> inObjWithProp "root"
+                                |> inObjWithProp "rootest"
+                                |> inObjWithProp "_"
+                                |> Ok
+                            )
+            , test "array" <|
+                \() ->
+                    NullValue
+                        |> inArray
+                        |> setIn [ "0" ] (StringValue "bar")
+                        |> Expect.equal
+                            (StringValue "bar"
+                                |> inArray
+                                |> Ok
+                            )
+            , test "array of arrays" <|
+                \() ->
+                    NullValue
+                        |> inArray
+                        |> inArray
+                        |> setIn [ "0", "0" ] (StringValue "bar")
+                        |> Expect.equal
+                            (StringValue "bar"
+                                |> inArray
+                                |> inArray
+                                |> Ok
+                            )
+            , test "array of arrays of arrays" <|
+                \() ->
+                    NullValue
+                        |> inArray
+                        |> inArray
+                        |> inArray
+                        |> setIn [ "0", "0", "0" ] (StringValue "bar")
+                        |> Expect.equal
+                            (StringValue "bar"
+                                |> inArray
+                                |> inArray
+                                |> inArray
+                                |> Ok
+                            )
+            , test "array of object" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "foo"
+                        |> inArray
+                        |> setIn [ "0", "foo" ] (StringValue "bar")
+                        |> Expect.equal
+                            (StringValue "bar"
+                                |> inObjWithProp "foo"
+                                |> inArray
+                                |> Ok
+                            )
+            , test "object with siblings" <|
+                \() ->
+                    NullValue
+                        |> inObjWithProp "foo"
+                        |> setIn [ "bar" ] (StringValue "bar")
+                        |> Expect.equal
+                            (Ok (ObjectValue ([ ( "foo", NullValue ), ( "bar", StringValue "bar" ) ])))
+            ]
+        , describe "setPropertyName"
+            [ test "simple object" <|
+                \() ->
+                    StringValue "bar"
+                        |> inObjWithProp "foo"
+                        |> setPropertyName ( [], 0 ) "bam"
+                        |> Expect.equal (Ok (ObjectValue ([ ( "bam", StringValue "bar" ) ])))
+            ]
+        ]
+
+
+inObjWithProp : String -> JsonValue -> JsonValue
+inObjWithProp name p =
+    ObjectValue [ ( name, p ) ]
+
+
+inArray : JsonValue -> JsonValue
+inArray p =
+    ArrayValue [ p ]
